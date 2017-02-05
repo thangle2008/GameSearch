6c7afe3e3e8a0745deb836e920d674efb591ecd4
Thang Le <thangminh.le208@gmail.com>
2017-02-05 18:33:11 -0500

    rearrange files and set up game search

--- webgame/settings.py
+++ webgame/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'homepage.apps.HomepageConfig',
+    'gamesearch.apps.GamesearchConfig',
     'djangobower',
     'django.contrib.admin',
     'django.contrib.auth',
