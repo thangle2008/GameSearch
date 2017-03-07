0e50713da3a17010f16aeb692db1e9bfeece5e4f
Thang Le <thangminh.le208@gmail.com>
2017-03-07 11:19:39 -0500

    config psql database

--- webgame/settings.py
+++ webgame/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,12 +78,8 @@ WSGI_APPLICATION = 'webgame.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', ''))
 }
-
 
 # Caches
 # https://docs.djangoproject.com/en/1.10/topics/cache/
